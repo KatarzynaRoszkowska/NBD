a21feb7145f9aa200f8dbfc206d707fef304dd70
KatarzynaRoszkowska <33752247+KatarzynaRoszkowska@users.noreply.github.com>
2020-04-26 11:07:41 +0200

    zapytanie 6

---
+++ CWICZENIE_3_s20841/zapytanie_6.js
@@ -0,0 +1,29 @@
+db.people.insertOne(
+{
+	"sex" : "Female",
+	"first_name" : "Katarzyna",
+	"last_name" : "Roszkowska",
+	"job" : "Developer",
+	"email" : "katarzyna.roszkowska19@gmail.com",
+	"location" : {
+		"city" : "Warszawa",
+		"address" : {
+			"streetname" : "Garbinskiego",
+			"streetnumber" : "34"
+		}
+	},
+	"description" : "d356 fo developer",
+	"height" : "169.10",
+	"weight" : "59.1",
+	"birth_date" : "1996-03-09T08:10:58Z",
+	"nationality" : "Poland",
+	"credit" : [
+		{
+			"type" : "jcb",
+			"number" : "2217950170321",
+			"currency" : "PLN",
+			"balance" : "363.86"
+		}
+	]
+}
+)
